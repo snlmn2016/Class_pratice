1f5277efec6307d834cf64c1a403c1ac2a669bf2
nazu <mohammed.nazu@yahoo.com>
2016-03-03 18:28:22 -0800

    Part Two

---
+++ IT_work/cricket_new2.rb
@@ -0,0 +1,14 @@
+require_relative "Cricket"
+
+Sachin = Cricket.new("Sachin", "All-Rounder", "India", 12345, 789)
+Dhoni = Cricket.new("Mahendra", "WK", "India", 2345, 0)
+Sabbir = Cricket.new("Rahman", "All-Rounder", "Bangladesh", 345, 9)
+Wasim = Cricket.new("Akram", "bowler", "Pakistan", 45, 999)
+
+getinfo.add_players(Sachin)
+getinfo.add_players(Dhoni)
+getinfo.add_players(Sabbir)
+getinfo.add_players(Wasim)
+
+
+getinfo.add_players
