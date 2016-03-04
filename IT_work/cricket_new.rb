734157bda5e151d1aa6a7dd0ba148d48704e1c49
nazu <mohammed.nazu@yahoo.com>
2016-03-03 18:26:48 -0800

    Part one

---
+++ IT_work/cricket_new.rb
@@ -0,0 +1,36 @@
+class Cricket
+@player
+@position
+@country	
+@runs
+@wickets
+
+def initialize(player, position, country, runs, wickets)
+@player = player
+@position = position
+@country = country
+@runs = runs
+@wickets = wickets
+end
+end
+
+def player
+@player
+end
+
+def position
+@position
+end
+
+def country
+@country
+end
+
+def runs
+@runs
+end
+
+def wickets
+@wickets
+end
+
