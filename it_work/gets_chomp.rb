b3463ba1d880237073666b7edd13853d6540cc29
nazu <mohammed.nazu@yahoo.com>
2016-03-21 02:35:59 -0700

    Gets.chomp pratice

---
+++ it_work/gets_chomp.rb
@@ -0,0 +1,19 @@
+puts "First Name?"
+firat_name = gets.chomp.capitalize
+
+puts "Last Name?"
+last_name = gets.chomp.capitalize
+
+puts "Age?"
+age = gets.chomp
+
+puts "Marital Status?"
+marital_status = gets.chomp
+
+puts "Carrer?"
+carrer = gets.chomp
+
+puts "#{firat_name} #{last_name} is #{age} years old. He is #{marital_status} and during the nights
+his job is #{carrer}."
+
+#gets.chomp pratice
