d15b8d39b6ceec0606a27723f3444754d53caa4f
nazu <mohammed.nazu@yahoo.com>
2016-03-21 02:43:00 -0700

    hash pratice

---
+++ it_work/hash_prac.rb
@@ -0,0 +1,12 @@
+player_name = Hash.new
+
+player_name["first_name"] = "Virat"
+player_name["last_name"] = "Kholi"
+player_name["team"] = "India"
+player_name["position"] = "Batsman"
+
+puts player_name["first_name"]
+puts player_name["team"]
+puts player_name["position"]
+
+#some hash pratice
