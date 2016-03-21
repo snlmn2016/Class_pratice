09ec619533afaa379d7e9939a8c56a31b49f597e
nazu <mohammed.nazu@yahoo.com>
2016-03-21 02:33:50 -0700

    Array Pratice

---
+++ it_work/array_prat.rb
@@ -0,0 +1,11 @@
+nfl_teams = ["NY Giants", "NY Jets", "Eagles", "Colts", "Patriots"]
+
+puts nfl_teams[3]
+
+puts "#{nfl_teams[0]} is my favorite football team. :)"
+
+puts "#{nfl_teams[4]} is the team I dislike the most. :("
+
+# Just a little array pratice.
+
+ 
