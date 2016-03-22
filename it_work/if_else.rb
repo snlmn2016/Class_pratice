b6dd13e89ad0c9be465d042fb8158139c954b79f
nazu <mohammed.nazu@yahoo.com>
2016-03-22 06:04:20 -0700

    if and else pratice

---
+++ it_work/if_else.rb
@@ -0,0 +1,13 @@
+points_socred = 107
+
+if points_socred >= 108
+	puts "The NY Knicks win the game."
+elsif points_socred < 88
+	puts "The Miami Heats loose the game."
+elsif points_socred == 104
+	puts "The game is tied."
+else
+	puts "This was a fake basketball game!!!"
+end
+ 
+#if and else pratice
