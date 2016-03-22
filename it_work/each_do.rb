69a6d4e69c029e3a1e606acb3b7e635ef796daca
nazu <mohammed.nazu@yahoo.com>
2016-03-22 06:01:09 -0700

    .each do pratice

---
+++ it_work/each_do.rb
@@ -0,0 +1,12 @@
+top_players = ["Tendulkar", "Kholi", "Dhoni", "Shakib", "Jadeja"]
+
+counter = 1
+
+top_players.each do |display|
+
+puts "#{counter}- #{display}"
+counter += 1
+
+end
+
+#.each do pratice
