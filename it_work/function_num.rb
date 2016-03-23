9a26611c70fc1f6e2fe0a4abb3c945981a053962
nazu <mohammed.nazu@yahoo.com>
2016-03-23 05:34:38 -0700

    function pratice

---
+++ it_work/function_num.rb
@@ -0,0 +1,17 @@
+def wrong_decisons(x)
+
+remainder = x % 4
+
+if remainder == 0
+	puts "The umpire gave all correct decisions."
+else
+	puts "This is an umpire that should be suspended."
+	end
+end
+
+wrong_decisons(20)
+wrong_decisons(25)
+wrong_decisons(100)
+wrong_decisons(123)
+
+#function pratice
